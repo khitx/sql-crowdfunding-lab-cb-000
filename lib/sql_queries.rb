5c14bb0335f931bb778818026d2225c59fe4f385
Kathy Hudson <khitx@icloud.com>
2018-05-02 15:44:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -2,7 +2,6 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   "SELECT projects.title, SUM(pledges.amount)
     FROM projects
     INNER JOIN pledges ON projects.id = pledges.project_id
-    INNER JOIN users ON pledges.user_id = users.id
     GROUP BY projects.id
     ORDER BY projects.title"
 end
